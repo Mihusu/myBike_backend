669d259269c94ee6615255c5da81b441ad2d11f1
jakobsaadbye <31799001+jakobsaadbye@users.noreply.github.com>
2023-02-14 16:20:15 +0100

    Bootstrapped simple example (Should be removed)

---
+++ controllers/controller_book.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Body, Request, Response, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from typing import List
+
+from models.model_book import Book
+
+router = APIRouter()
+
+@router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
+def create_book(request: Request, book: Book = Body(...)):
+    book = jsonable_encoder(book)
+    new_book = request.app.database["books"].insert_one(book)
+    created_book = request.app.database["books"].find_one(
+        {"_id": new_book.inserted_id}
+    )
+
+    return created_book
+
+
+@router.get("/", response_description="List all books", response_model=List[Book])
+def list_books(request: Request):
+    books = list(request.app.database["books"].find(limit=100))
+    return books
+
+
+@router.get("/{id}", response_description="Get a single book by id", response_model=Book)
+def find_book(id: str, request: Request):
+    if (book := request.app.database["books"].find_one({"_id": id})) is not None:
+        return book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
+
+
+@router.delete("/{id}", response_description="Delete a book")
+def delete_book(id: str, request: Request, response: Response):
+    delete_result = request.app.database["books"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from typing import Union
-
+from dotenv import dotenv_values
+from pymongo import MongoClient
 from fastapi import FastAPI
+import certifi
+
+from controllers.controller_book import router
 
 app = FastAPI()
 
+config = dotenv_values(".env")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.on_event("startup")
+def startup_db_client():
+    print("Setting up database...")
+    
+    # For some reason, the database connection fails with SSL: CERTIFICATE_VERIFY_FAILED
+    # when not using tlsCAFile=certifi.where(), so i am keeping it here for now until 
+    # a new solution is found
+    app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
+    app.database = app.mongodb_client[config["DB_NAME"]]
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    print("Closing database...")
+    app.mongodb_client.close()
+    
+
+app.include_router(router, tags=["books"], prefix="/books")
