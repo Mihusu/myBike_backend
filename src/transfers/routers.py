68c9e6638e58f7a8849fd647606f3dee9a9d4456
sture <sturess@gmail.com>
2023-03-20 17:14:24 +0100

    reversed logic

--- src/transfers/routers.py
+++ src/transfers/routers.py
@@ -62,7 +62,7 @@ def create_transfer(
 @router.post('/{:id}/accept', description="Accepts a bike transfer", status_code=status.HTTP_202_ACCEPTED)
 def accept_transfer(
     request: Request, 
-    sender: BikeOwner = Depends(authenticated_request), 
+    requester: BikeOwner = Depends(authenticated_request), 
     bike_id: uuid.UUID = Depends(bike_with_id_exists),
     transfer_id: uuid.UUID = Depends(transfer_with_id_exists)
 ) -> BikeTransfer:
@@ -73,18 +73,17 @@ def accept_transfer(
     if bike.reported_stolen:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Bike is reported stolen. Transfer disallowed")
 
-    # Checks the transfer exists and not closed
-    transfer_in_db = request.app.collections["transfers"].find_one({"_id": transfer_id})
-    transfer = BikeTransfer(**transfer_in_db)
-    if bike.state != BikeState.IN_TRANSFER and transfer.state != BikeTransferState.CLOSED:
+    # Checks the transfer state is in transfer and is pending
+    transfer = BikeTransfer(**transfer_id)
+    if bike.state != BikeState.IN_TRANSFER or transfer.state != BikeTransferState.PENDING:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Bike is not in transfer")
     
     # Checks if the requester is also the receiver from a transfer
-    if sender.id != transfer.sender:
+    if requester.id != transfer.receiver:
         raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail=f"The receiver is not the same person in the transfer")
     
     # This hands over the ownership to the sender of the request
-    bike.owner = sender.id
+    bike.owner = requester.id
     bike.state = BikeState.TRANSFERABLE
     bike.claimed_date = datetime.datetime.now()
     transfer.state = BikeTransferState.CLOSED
