1814ae3e48b23c91159d68319b65e2695ba4376f
Mihusu <ming2200@hotmail.com>
2023-03-27 10:13:11 +0200

    Utils updates

--- src/transfers/utils.py
+++ src/transfers/utils.py
@@ -8,12 +8,13 @@ def expand_transfer(transfer: BikeTransfer, request: Request) -> dict:
     """ Serializes a single bike transfer to expand ids into full objects """
     
     sender   = BikeOwner(**request.app.collections['bike_owners'].find_one({'_id' : transfer.sender})).dict(include={'_id', 'phone_number'})
-    reciever = BikeOwner(**request.app.collections['bike_owners'].find_one({'_id' : transfer.receiver})).dict(include={'_id', 'phone_number'})
+    receiver = BikeOwner(**request.app.collections['bike_owners'].find_one({'_id' : transfer.receiver})).dict(include={'_id', 'phone_number'})
     bike     = Bike(**request.app.collections['bikes'].find_one({'_id' : transfer.bike_id})).dict(exclude={'receipt'})
     
     return {
+        'transfer_id': transfer.id,
         'sender'     : sender,
-        'receiver'   : reciever,
+        'receiver'   : receiver,
         'bike'       : bike,
         'created_at' : transfer.created_at,
         'closed_at'  : transfer.closed_at,
